330b39494093760628bceeca9b24ac2380413d3e
Mica Lorraine D. Porras <micalorrainedporras@iskolarngbayan.pup.edu.ph>
2026-03-13 22:46:43 +0800

    chore: initial website setup

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Google_Sans } from "next/font/google";
+import "./globals.css";
+
+const googleSans = Google_Sans({
+  variable: "--font-google-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Personal Website",
+  description: "A personal website built with Next.js and Tailwind CSS.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${googleSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
